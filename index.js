14a043a91412a403c1e49b65d4d3ea721adc2c58
philipe <philipedarosa10@gmail.com>
2025-08-04 13:29:47 -0300

    input

---
+++ index.js
@@ -0,0 +1,13 @@
+let input = document.getElementById("input-principal")
+let button = document.getElementById("botão-adicionar")
+let button2 = document.getElementById("botão-remover")
+
+
+
+function cliqueBotao(){
+    console.log(input.value)
+
+}
+
+
+button.addEventListener("click", cliqueBotao)
