1e205aa67b9b967484b9964ce682bc604f646fce
philipe <philipedarosa10@gmail.com>
2025-08-04 23:41:56 -0300

    pronto

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 let input = document.getElementById("input-principal")
 let button = document.getElementById("botão-adicionar")
 let button2 = document.getElementById("botão-remover")
+let listaDeTarefas = document.getElementById("tarefas")
+
+let arryDeTarefas = []
 
 
 
 function cliqueBotao(){
-    console.log(input.value)
+    arryDeTarefas.push(input.value)
+    mostrarTarefas()
+}
+function mostrarTarefas(){
+    let novaLista = ""
 
+    arryDeTarefas.forEach(tarefa => {
+        novaLista = novaLista + `
+            <li class="item-tarefa">
+                <p>${tarefa}</p>
+            </li>
+
+        `
+        
+
+    }) 
+   listaDeTarefas.innerHTML = novaLista
+    
 }
 
 
